a8b30881cec11edbe8d39dbe7463ae8b548f8b17
lucaschneider@gmail.com <lucaschneider@gmail.com>
2023-09-01 19:25:43 -0300

    actualizaciones varias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', canActivate: [AuthGuard]},
+  { path: '**', redirectTo: '' }
+];
 
-@NgModule({
+@NgModule({ 
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dacs';
+export class AppComponent implements OnInit {
+  title = 'dacs2023';
+  public isLogueado = false;
+  public perfilUsuario: KeycloakProfile | null = null;
+  public role = false;
+  constructor(private readonly keycloak: KeycloakService) {}
+
+  public async ngOnInit() {
+
+    this.isLogueado = await this.keycloak.isLoggedIn();
+    this.role=await this.keycloak.isUserInRole("ROLE-A");
+    console.log ("role=====>", this.role );
+    if(this.isLogueado && !this.role){
+      this.keycloak.logout();
+      return;
+    }
+    type rolesUsuarios = Array<{id: number, text: string}>;
+
+    if (this.isLogueado) {
+      this.perfilUsuario = await this.keycloak.loadUserProfile();
+    }
+  }
+
+  public iniciarSesion() {
+    this.keycloak.login();
+  }
+
+  public cerrarSesion() {
+    this.keycloak.logout();
+  }
 }
